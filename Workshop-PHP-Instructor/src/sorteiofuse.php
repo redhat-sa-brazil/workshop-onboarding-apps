5cd97cb67d4c027bd368c42075000e3707f98424
Luciano Scorsin <lucianoscorsin@iMac-de-Luciano.local>
2018-04-30 17:34:17 -0300

    Troca por modal large

--- Workshop-PHP-Instructor/src/sorteiofuse.php
+++ Workshop-PHP-Instructor/src/sorteiofuse.php
@@ -135,7 +135,7 @@ require_once("top_navigation.php");
                       </div>
 	 		<div class="form-group">
 
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">SSH GCE <span class="required">*</span>
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Participantes
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
                           <textarea id="alunos" name="alunos" required="required" class="form-control" name="message" rows="20"><?php echo $alunos;?></textarea>
@@ -174,23 +174,20 @@ require_once("top_navigation.php");
                   <!-- Large modal -->
                   <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
 
-                  <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+                  <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true" id="modalgrande">
                     <div class="modal-dialog modal-lg">
                       <div class="modal-content">
 
                         <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
                           </button>
-                          <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                          <h4 class="modal-title" id="myModalLabel">Parabens!</h4>
                         </div>
                         <div class="modal-body">
-                          <h4>Text in a modal</h4>
-                          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-                          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+                          <h4><?php echo $sorteado;?></h4>        
                         </div>
                         <div class="modal-footer">
                           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                          <button type="button" class="btn btn-primary">Save changes</button>
                         </div>
 
                       </div>
@@ -282,7 +279,7 @@ require_once("top_navigation.php");
     
     <script>
         function AbreModal() {
-            $('#modalpequena').modal('show');
+            $('#modalgrande').modal('show');
             
         }
         </script>
