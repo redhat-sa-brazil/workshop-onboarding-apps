e96487ef5236184fd8e2871e6b5557f4be1bc829
lasherbr <ricardi@gmail.com>
2018-12-28 11:59:18 -0200

    Fixes em paths

--- Workshop-PHP-Instructor/assets/entrypoint.sh
+++ Workshop-PHP-Instructor/assets/entrypoint.sh
@@ -1,9 +1,9 @@
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
-envsubst < /tmp/passwd.template > /tmp/passwd
+envsubst < /app/passwd.template > /app/passwd
 export LD_PRELOAD=libnss_wrapper.so
-export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_PASSWD=/app/passwd
 export NSS_WRAPPER_GROUP=/etc/group
-mkdir /tmp/home
-chown -R $USER_ID /tmp/home
+mkdir /app/home
+chown -R $USER_ID /app/
 /usr/sbin/httpd -D FOREGROUND
